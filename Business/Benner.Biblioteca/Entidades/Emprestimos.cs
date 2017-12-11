506827c648e327514a4b315b94e43eeee3fab958
M014 <M014@benner.com.br>
2017-12-11 11:58:42 -0200

    Devolução de Livros e Controle de Empréstimo

--- Business/Benner.Biblioteca/Entidades/Emprestimos.cs
+++ Business/Benner.Biblioteca/Entidades/Emprestimos.cs
@@ -12,13 +12,50 @@ using System.Text;
 
 namespace Benner.Biblioteca.Entidades
 {
-    
-    
+
+
     /// <summary>
     /// Nome da Tabela: EMPRESTIMOS.
     /// Essa é uma classe parcial, os atributos, herança e propriedades estão definidos no arquivo Emprestimos.properties.cs
     /// </summary>
     public partial class Emprestimos
     {
+        protected override void Saving()
+        {
+            var livroDao = LivrosDao.CreateInstance();
+            var livro = livroDao.Get(LivroHandle);
+            if (livro.Emprestado == true)
+            {
+                throw new BusinessException("Este livro já está emprestado.");
+            }
+            else
+            {
+                livro.Emprestado = true;
+                livroDao.Save(livro);
+            }
+            base.Saving();
+        }
+
+        public void Devolver(BusinessArgs args)
+        {
+            var emprestimoDao = EmprestimosDao.CreateInstance();
+            var emprestimo = emprestimoDao.Get(Handle);
+            var livroDao = LivrosDao.CreateInstance();
+            var livro = livroDao.Get(LivroHandle);
+            if (livro.Emprestado == true)
+            {
+                emprestimo.DataDevolucao = DateTime.Now.Date;
+                livro.Emprestado = false;
+                livroDao.Save(livro);
+                emprestimoDao.Save(emprestimo);
+            }
+            else
+            {
+                throw new BusinessException("Este livro já foi devolvido.");
+            }
+
+
+           
+        }
     }
 }
